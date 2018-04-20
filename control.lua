525c0112ec053b3f5ee4a00f97290800207c9019
GalactusX31 <36307167+GalactusX31@users.noreply.github.com>
2018-04-20 09:17:59 +0200

    Add files via upload

--- control.lua
+++ control.lua
@@ -1,5 +1,5 @@
 --[[
-  "Keybindused" is only used to control the script (on_gui_opened), so it only work when his value is 0,
+  "Keybindused" is only to control the script (on_gui_opened), so it only work when his value is 0,
   and prevent to close the gui when the keybin key is presed to open the entity gui
 ]]
 local keybindused = 0
@@ -25,16 +25,19 @@ end
 local function onKey(event)
   local player = game.players[event.player_index]
   local entity = player.selected
+
   keybindused = 1
-  if entity and entity.valid and entity.name == 'switchbutton' then
-    local distance = math.abs(player.position.x-entity.position.x)+math.abs(player.position.y-entity.position.y)
-    if distance < 15 or player.character == nil then
-      local control = entity.get_or_create_control_behavior()
-      if settings.startup['ReverseOpenInventory'].value then
-        player.opened = entity
-        keybindused = 0
-      elseif not settings.startup['ReverseOpenInventory'].value then
-        control.enabled = not control.enabled
+  if entity and entity.valid then
+    if entity.name == 'switchbutton' then
+      local distance = math.abs(player.position.x-entity.position.x)+math.abs(player.position.y-entity.position.y)
+      if distance < 15 or player.character.can_reach_entity('switchbutton') then
+        local control = entity.get_or_create_control_behavior()
+        if settings.startup['ReverseOpenInventory'].value then
+          player.opened = entity
+          keybindused = 0
+        elseif not settings.startup['ReverseOpenInventory'].value then
+          control.enabled = not control.enabled
+        end
       end
     end
   end
